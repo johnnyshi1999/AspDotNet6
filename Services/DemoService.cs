f408a2adb1a8b956b548dad941458739926cb204
johnnyshi1999 <johnnyshi99@gmail.com>
2021-09-09 14:30:57 +0700

    Add project files.

---
+++ Services/DemoService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspDotNet6.Service
+{
+    public class DemoService : IDemoService
+    {
+        private static readonly string[] Locations = new[]
+        {
+            "Hanoi", "Washington", "Sydney"
+        };
+        public string GetLocationById(int i)
+        {
+            try
+            {
+                return Locations[i];
+            } catch(Exception)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
